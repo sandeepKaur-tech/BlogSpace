7f65d6a5b4c6b7a61db08824dca9390f8e48a1b1
sandeepKaur-tech <kaur94sandeep@gmail.com>
2024-08-29 17:57:28 +0400

    Send new post to the serverQ

--- index.js
+++ index.js
@@ -14,3 +14,26 @@ fetch("https://apis.scrimba.com/jsonplaceholder/posts")
         console.log(content)
         document.getElementById("blog-list").innerHTML = content
       })
+
+      document.getElementById("new-post").addEventListener("submit", function(e) {
+        e.preventDefault()
+        const postTitle = document.getElementById("post-title").value
+        const postBody = document.getElementById("post-body").value
+        const data = {
+          title : postTitle,
+          body : postBody
+        }
+        const options = {
+          method: "POST",
+          body: JSON.stringify(data),
+          headers: {
+              "Content-Type": "application/json"
+          }
+      }
+      
+      fetch("https://apis.scrimba.com/jsonplaceholder/posts", options)
+          .then(res => res.json())
+          .then(data => console.log(data))
+    
+    })
+   
